9e8783f8724839c6236fb2ea2510c8de479a923b
ZhuLiya <k1781927345@gmail.com>
2016-08-01 02:31:33 -0700

    2016.7.31

---
+++ mywebsite/scripts.js
@@ -0,0 +1,15 @@
+// window.onload = function(){
+// 	alert('welcome to my page!!!');
+// }
+
+// $(links).ready(function(){
+//     $("#p1").mouseenter(function(){
+//         alert("You entered p1!");
+//     });
+// });
+
+// document.getElementById("myBtn").onclick = function() {myFunction()};
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+
